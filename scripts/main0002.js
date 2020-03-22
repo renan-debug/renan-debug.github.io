9d940ea39d3815525d3adfa1c41fbbe57d390ceb
renan-debug <renan-debug@users.noreply.github.com>
2020-03-22 19:18:59 -0300

    002

---
+++ scripts/main0002.js
@@ -0,0 +1,9 @@
+document.querySelector('img').onclick = function() {
+    alert('Isso é uma xícara. Você perdeu.');
+}
+document.querySelector("#certo").onclick = function() {
+    alert('Temos um xeroque rolmes aqui. Você ganhou.');
+}
+document.querySelector("#errado").onclick = function() {
+    alert('Aprenda a ler. Você perdeu.');
+}
