92f410015964307b2329561b8ec546e736ba7c16
renan-debug <renan-debug@users.noreply.github.com>
2020-03-25 13:49:46 -0300

    Update main0004.js

--- scripts/main0004.js
+++ scripts/main0004.js
@@ -46,12 +46,15 @@ var mensagemStatus = [
 	"Insatisfeito", 
 	"Neutro",
 	"Satisfeito"];
-
+//Com esses valores nas variáveis de botão. A primeira mensagem de tela será a do início do jogo: "Não surte na quarentena!!", 
+//se for qualquer número menor que a quantidade total de mensagens, a primeira mensagem de tela estará errada.
 var botao1MensagemAtual = 32;
 var botao2MensagemAtual = 32;
 var botao3MensagemAtual = 32;
+//O valor dessa variável deve ser 50 para iniciar o jogo no status neutro.
 var barraStatus = 50;
 
+//A parte abaixo define o que acontece quando se clica em cada botão.
 document.querySelector("#botaoInteracao1").onclick = function() {
 	atualizarStatus(1);
 	atualizaMensagemTela(botao1MensagemAtual);
@@ -67,6 +70,7 @@ document.querySelector("#botaoInteracao3").onclick = function() {
 	atualizaMensagemTela(botao3MensagemAtual);
 	tirarCartas();
 }
+//A função abaixo tira uma carta aleatória que pode ser repetitida.
 function tirarCartas() {
 	botao1MensagemAtual = Math.floor((Math.random() * 32));
 	document.querySelector("#botaoInteracao1").innerHTML = mensagemBotoes[botao1MensagemAtual];
@@ -75,7 +79,9 @@ function tirarCartas() {
 	botao3MensagemAtual = Math.floor((Math.random() * 32));
 	document.querySelector("#botaoInteracao3").innerHTML = mensagemBotoes[botao3MensagemAtual];
 }
+//A função abaixo atualiza o Status. 
 function atualizarStatus(numeroBotao) {
+	//O cálculo da variável "a" abaixo pega o índice da array que guarda a msg atual do botão e divide pela quantidade total de msg's de botão para obter um número entre 0 e 1. 
 	if(numeroBotao === 1){
 		var a=(botao1MensagemAtual+1)/32;
 		calculaStatus(a);
@@ -89,10 +95,13 @@ function atualizarStatus(numeroBotao) {
 		calculaStatus(a);
 	}
 }
+//A função abaixo é utilizada na função acima, ela calcula o novo valor numérico e o nome correspondente a essa quantidade de pontos.
 function calculaStatus(valorA){
+	//Se o valor for maior que 1, significa que é a mensagem inicial do jogo.
 	if(valorA>1){
 		document.querySelector("#status").innerHTML = "Status: Neutro ("+barraStatus+")";
 	}
+	//Se o valor é maior que 0.5 significa que são as mensagens que tiram pontos. Então tiramos 0.5
 	else if(valorA>0.5){
 		valorA=Math.floor((valorA-0.5)*(-8));
 		barraStatus=barraStatus+valorA;
@@ -101,6 +110,7 @@ function calculaStatus(valorA){
 		valorA=Math.floor(valorA*8);
 		barraStatus=barraStatus+valorA;
 	}
+	//O cálculo abaixo é igual ao valor máximo da barra de status dividido pela quantidade de mensagens de status.
 	indiceMsgStatus=Math.floor(barraStatus/25);
 	if(indiceMsgStatus>=4){
 		alert("Você ganhou!");
@@ -114,7 +124,9 @@ function calculaStatus(valorA){
 		location.reload();
 	}
 }
+//A função abaixo atualiza a mensagem da Tela.
 function atualizaMensagemTela(indiceMsgBotao){
+	//O cáculo abaixo é igual ao índice da mensagem do botão dividido pelo número de mensagens de tela. 
 	indiceMsgBotao = Math.floor(indiceMsgBotao/4);
 	document.querySelector("#tela").innerHTML = mensagemTela[indiceMsgBotao];
 }
