8171013127e1f05093bda1535a8ccbc8fd2f4029
renan-debug <renan-debug@users.noreply.github.com>
2020-03-25 12:59:30 -0300

    Update main0004.js

--- scripts/main0004.js
+++ scripts/main0004.js
@@ -1,12 +1,36 @@
 var mensagemBotoes = [
-	"Apagar arquivos antigos inúteis",
-	"Imaginar uma história divertida",
-	"Jogar no computador", 
-	"Meditar",
-	"Pensar no pior",
-	"Só comer besteira",
-	"Quebrar as coisas em casa",
-	"Chorar com medo"];
+	'Aprender algo novo',
+	'Começar um novo projeto',
+	'Adiantar trabalho',
+	'Apagar arquivos antigos inúteis',
+	'Jogar fora coisas que não usa',
+	'Organizar a bagunça',
+	'Ler ',
+	'Imaginar uma história divertida',
+	'Desligar a tv',
+	'Assistir filmes',
+	'Assistir séries',
+	'Jogar no computador ',
+	'Cozinhar',
+	'Retomar contato com amigos',
+	'Conversar nas redes sociais',
+	'Meditar',
+	'Ficar de pijama o dia todo',
+	'Passar o dia na cama',
+	'Ficar no escuro',
+	'Pensar no pior',
+	'Ir no supermercado',
+	'Dormir demais',
+	'Deixar de tomar banho',
+	'Só comer besteira',
+	'Comprar toneladas de papel higiênico',
+	'Sair para uma caminhada',
+	'Espalhar fake news',
+	'Quebrar as coisas em casa',
+	'Reclamar por não poder sair',
+	'Combinar um churrasco com a galera',
+	'Criar intriga com as pessoas de casa',
+	'Chorar com medo'];
 var mensagemTela = [
 	"Você se sente um pouquinho melhor", 
 	"Você se sente um pouco melhor", 
@@ -23,45 +47,45 @@ var mensagemStatus = [
 	"Neutro",
 	"Satisfeito"];
 
-var botao1MensagemAtual = 8;
-var botao2MensagemAtual = 8;
-var botao3MensagemAtual = 8;
+var botao1MensagemAtual = 32;
+var botao2MensagemAtual = 32;
+var botao3MensagemAtual = 32;
 var barraStatus = 50;
 
 document.querySelector("#botaoInteracao1").onclick = function() {
 	atualizarStatus(1);
-	document.querySelector("#tela").innerHTML = mensagemTela[botao1MensagemAtual];
+	atualizaMensagemTela(botao1MensagemAtual);
 	tirarCartas();
 }
 document.querySelector("#botaoInteracao2").onclick = function() {
 	atualizarStatus(2);
-	document.querySelector("#tela").innerHTML = mensagemTela[botao2MensagemAtual];
+	atualizaMensagemTela(botao2MensagemAtual);
 	tirarCartas();
 }
 document.querySelector("#botaoInteracao3").onclick = function() {
 	atualizarStatus(3);
-	document.querySelector("#tela").innerHTML = mensagemTela[botao3MensagemAtual];
+	atualizaMensagemTela(botao3MensagemAtual);
 	tirarCartas();
 }
 function tirarCartas() {
-	botao1MensagemAtual = Math.floor((Math.random() * 8));
+	botao1MensagemAtual = Math.floor((Math.random() * 32));
 	document.querySelector("#botaoInteracao1").innerHTML = mensagemBotoes[botao1MensagemAtual];
-	botao2MensagemAtual = Math.floor((Math.random() * 8));
+	botao2MensagemAtual = Math.floor((Math.random() * 32));
 	document.querySelector("#botaoInteracao2").innerHTML = mensagemBotoes[botao2MensagemAtual];
-	botao3MensagemAtual = Math.floor((Math.random() * 8));
+	botao3MensagemAtual = Math.floor((Math.random() * 32));
 	document.querySelector("#botaoInteracao3").innerHTML = mensagemBotoes[botao3MensagemAtual];
 }
 function atualizarStatus(numeroBotao) {
 	if(numeroBotao === 1){
-		var a=(botao1MensagemAtual+1)/8;
+		var a=(botao1MensagemAtual+1)/32;
 		calculaStatus(a);
 	}
 	else if(numeroBotao === 2){
-		var a=(botao2MensagemAtual+1)/8;
+		var a=(botao2MensagemAtual+1)/32;
 		calculaStatus(a);
 	}
 	else{
-		var a=(botao3MensagemAtual+1)/8;
+		var a=(botao3MensagemAtual+1)/32;
 		calculaStatus(a);
 	}
 }
@@ -70,31 +94,27 @@ function calculaStatus(valorA){
 		document.querySelector("#status").innerHTML = "Status: Neutro ("+barraStatus+")";
 	}
 	else if(valorA>0.5){
-		valorA=(valorA-0.5)*(-8);
+		valorA=Math.floor((valorA-0.5)*(-8));
 		barraStatus=barraStatus+valorA;
 	}
 	else{
-		valorA=valorA*8;
+		valorA=Math.floor(valorA*8);
 		barraStatus=barraStatus+valorA;
 	}
-	if(barraStatus>=100){
+	indiceMsgStatus=Math.floor(barraStatus/25);
+	if(indiceMsgStatus>=4){
 		alert("Você ganhou!");
 		location.reload();
 	}
-	else if(barraStatus>=75){
-		document.querySelector("#status").innerHTML = "Status: "+mensagemStatus[3]+" ("+barraStatus+")";
-	}
-	else if(barraStatus>=50){
-		document.querySelector("#status").innerHTML = "Status: "+mensagemStatus[2]+" ("+barraStatus+")";
-	}
-	else if(barraStatus>=25){
-		document.querySelector("#status").innerHTML = "Status: "+mensagemStatus[1]+" ("+barraStatus+")";
-	}
-	else if(barraStatus>0){
-		document.querySelector("#status").innerHTML = "Status: "+mensagemStatus[0]+" ("+barraStatus+")";
+	else if ((indiceMsgStatus<4) &&(indiceMsgStatus>=0)){
+		document.querySelector("#status").innerHTML = "Status: "+mensagemStatus[indiceMsgStatus]+" ("+barraStatus+")";
 	}
 	else{
 		alert("Você surtou!");
 		location.reload();
 	}
+}
+function atualizaMensagemTela(indiceMsgBotao){
+	indiceMsgBotao = Math.floor(indiceMsgBotao/4);
+	document.querySelector("#tela").innerHTML = mensagemTela[indiceMsgBotao];
 }
